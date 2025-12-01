2f28cd547747f4530f1b43c1eafbd47b79e4b864
bandajo094@gmail.com <joshua>
2025-12-01 00:42:40 -0800

    aa

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,19 +1,37 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer ,GeoJSON} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+import roadsData from "../data/roads.json";
 export default function Map() {
+
+  const roadStyle=(feature)=>({
+    color:feature.properties.security_status ==="secure"? "green":"red",
+    weight:4,
+  })
   return (
     <MapContainer
      center={[-15.3850, 35.3240]}
       zoom={13}
       scrollWheelZoom={true}
-      style={{ height: "500px", width: "100%" }}  // Required for visibility
+      style={{ height: "500px", width: "100%" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+
+
+      <GeoJSON
+        data={roadsData}
+        style={roadStyle}
+        onEachFeature={(feature, layer) => {
+          layer.bindPopup(`
+            <strong>${feature.properties.road_name}</strong><br/>
+            Status: ${feature.properties.security_status}
+          `);
+        }}
       />
     </MapContainer>
   );
